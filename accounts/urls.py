2666bfdaa6b3f3de5f0bec368d51c7a3ce36223c
daiki.miyazato <daiki.miyazato03@gmail.com>
2025-11-27 23:48:23 +0900

    ccounts(アカウント機能を実装)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
-#アプリフォルダ名{ %'アプリ名':パスのニックネーム % }
-from django.urls import path,include
-
-app_name = 'accounts' 
+app_name = 'accounts'
 
 urlpatterns = [
-    #デフォルトの認証URL
+    path('create/', views.create, name='create'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('logout/', views.logout_view, name='logout'), 
     path('', include('django.contrib.auth.urls')),
-    #ユーザー登録ページ
-    path('cleate/', views.cleate, name='cleate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserCreateForm
+from django.contrib.auth import logout
 
-# Create your views here.
+# アカウント作成
+def create(request):
+    if request.method == "POST":
+        # フォームのデータを取得
+        form = UserCreateForm(request.POST)
+
+        if form.is_valid():
+
+            # DBに保存せずユーザーオブジェクトを作成（commit=False）
+            user = form.save(commit=False)
+            user.is_staff = False                   # 管理画面アクセス権限を無効化（一般ユーザー）
+            user.is_superuser = False               # スーパーユーザー権限を無効化
+            user.save()                             # ユーザー情報をDBに保存
+            return redirect("accounts:login")       # ログインページへ
+    else:
+        # GETリクエスト（初回アクセス時）の処理(空のフォームを作成)
+        form = UserCreateForm()
+
+    return render(request, "accounts/create.html", {"form": form})
+
+# マイページ
+def mypage(request):
+    return(render(request, "accounts/mypage.html"))
+
+# ログアウト
+def logout_view(request):
+    logout(request)
+    return redirect('home:index') 
+
